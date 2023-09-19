f25799aa010d5dd37328fcbf5000bb2053075fed
Farahalh <120371923+Farahalh@users.noreply.github.com>
2023-09-19 16:55:40 +0200

    Building the Customer class and adding some of it´s properties

---
+++ ACM.BL/Customer.cs
@@ -0,0 +1,20 @@
+﻿namespace ACM.BL
+{
+    public class Customer
+    {
+        public string FirstName { get; set; }
+
+        private string _lastName;
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                _lastName = value;
+            }
+        }
+    }
+}
