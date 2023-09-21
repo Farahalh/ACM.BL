9eb83a2a78b7a6975650fd29b40e3eedaf493e46
Farahalh <120371923+Farahalh@users.noreply.github.com>
2023-09-21 09:38:16 +0200

    Adding more props

--- ACM.BL/Customer.cs
+++ ACM.BL/Customer.cs
@@ -2,7 +2,17 @@
 {
     public class Customer
     {
+        public int CustomerId { get; private set; }
+        public string EmailAdress { get; set; }
         public string FirstName { get; set; }
+
+        public string FullName 
+        { 
+            get
+            {
+                return LastName + " " + FirstName;
+            }
+        }
 
         private string _lastName;
         public string LastName
